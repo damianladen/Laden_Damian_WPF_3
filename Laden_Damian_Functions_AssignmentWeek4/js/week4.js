c2385426c370df9ac3ec5780ab9974d40e67397b
Damian Laden <damiananthony@fullsail.eduS>
2015-04-30 17:48:03 -0700

    Functions Assignment
    commit1

---
+++ Laden_Damian_Functions_AssignmentWeek4/js/week4.js
@@ -0,0 +1,12 @@
+// Damian Laden
+// 4/30/2015
+// Functions Assignment Week 4
+
+// Total calories eaten in a day
+var total = 0;
+
+// Calculate the total amount eaten for each meal
+function totalCalories(breakfast, lunch, dinner){
+    return parseInt(a) + parseInt(b) + parseInt(c);
+}
+
