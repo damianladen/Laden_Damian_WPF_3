f9b0c7e1552a78568c94fdbe69e63967bcc3102c
Damian Laden <damiananthony@fullsail.eduS>
2015-04-30 17:50:20 -0700

    Functions Assignment
    commit3

--- Laden_Damian_Functions_AssignmentWeek4/js/week4.js
+++ Laden_Damian_Functions_AssignmentWeek4/js/week4.js
@@ -10,3 +10,21 @@ function totalCalories(breakfast, lunch, dinner){
     return parseInt(a) + parseInt(b) + parseInt(c);
 }
 
+// Get user input
+var a = prompt("How many calories did you eat at breakfast?"),
+    b = prompt("How many calories did you eat at lunch?"),
+    c = prompt("How many calories did you eat for dinner?");
+
+// Check user input
+while(a && b && c){
+
+    // Calculate total calories for the day using user input
+    total = totalCalories(a, b, c);
+
+    // Figure out what percent this is of the daily recommended total
+    var totalPercent = function(t){
+
+        // Calculate percentage of total based on a 2000 calorie/day diet
+        return (t / 2000) * 100;
+
+    }
