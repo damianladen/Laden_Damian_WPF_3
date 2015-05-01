f2b6cfc2a9156a63985aeb2d05a4b7cbf2402517
Damian Laden <damiananthony@fullsail.eduS>
2015-04-30 17:51:50 -0700

    functions assignment
    commit5

--- Laden_Damian_Functions_AssignmentWeek4/js/week4.js
+++ Laden_Damian_Functions_AssignmentWeek4/js/week4.js
@@ -28,3 +28,16 @@ while(a && b && c){
         return (t / 2000) * 100;
 
     }
+
+    // Print the calorie information
+    console.log("Today you ate " + total + " calories, this is " + totalPercent(total) + "% of the daily recommended amount");
+
+    // Leave the while loop
+    break;
+}
+
+/* I typed in 500 for breakfast, 500 for lunch and 600 for dinner and it gave me 1600 calories total and it gave me 80% for the daily percentage
+ */
+
+/* When I entered 600 for breakfast, 900 for lunch and 800 for dinner it gave me 2300 calories total and 114.9999% for the daily total.
+ */
